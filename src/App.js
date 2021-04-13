5eaf316ef1b96bf5b713e4737785d844cb28af6e
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2021-04-13 00:50:58 -0400

    Using Konva

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, useState, useEffect, useRef, useCallback } from 'react'
+import Konva from 'konva'
 import firebase, { auth, provider } from './firebase.js'
 import Canvas from './Canvas'
 import Toolbar from './Toolbar'
@@ -10,41 +11,119 @@ const App = () => {
         gridMode : false,
         color: '#00F'
     })
+    const containerRef = useRef()
+    const [stage, setStage] = useState(null)
     const [currentItem, setCurrentItem] = useState('')
-    const [items, setItems] = useState([])
+    const [users, setUsers] = useState([])
     const [user, setUser] = useState(null)
-
-    const listenToItemsCollection = () => {
-        const itemsRef = firebase.database().ref('users');
-        itemsRef.on('value', (snapshot) => {
-            let users = snapshot.val()
-            let newState = []
-
-            for (let userId in users) {
-                let user = users[userId]
-                let items = user.items
-                for (let item in items) {
-                    newState.push({
-                        id: item,
-                        uid: userId,
-                        title: items[item].title,
-                        user: items[item].user
-                    })
-                }
-            }
-
-            setItems(newState)
-        })
-    }
 
     useEffect(() => {
         auth.onAuthStateChanged((userObj) => {
             if (userObj) {
                 setUser(userObj)
-                listenToItemsCollection()
+
+                const usersRef = firebase.database().ref('users');
+                usersRef.on('value', (snapshot) => {
+                    let users = snapshot.val()
+                    let newState = []
+
+                    for (let userId in users) {
+                        let user = users[userId]
+                        let items = user.items
+                        for (let item in items) {
+                            newState.push({
+                                id: item,
+                                uid: userId,
+                                title: items[item].title,
+                                user: items[item].user
+                            })
+                        }
+                    }
+
+                    setUsers(newState)
+                })
+
+                setStage(new Konva.Stage({
+                    container: 'canvas',
+                    width: containerRef.current.clientWidth,
+                    height: containerRef.current.clientHeight
+                }))
             } 
         })
     }, [])
+
+    useEffect(() => {
+        if(!stage)
+            return
+
+        const itemsRef = firebase.database().ref(`canvas/test/items`)
+        itemsRef.get().then(snapshot => syncCanvas(snapshot.val()))
+        itemsRef.on('value', snapshot => syncCanvas(snapshot.val()))
+    },[stage])
+
+    const syncCanvas = (items) => {
+        let newObjects = []
+
+        var layer = new Konva.Layer()
+
+        for (let item in items) {
+            if(item){
+                newObjects.push({
+                    id: item,
+                    x: items[item].x,
+                    y: items[item].y
+                })
+
+                var circle = new Konva.Circle({
+                    x: items[item].x,
+                    y: items[item].y,
+                    radius: 70,
+                    fill: 'red',
+                    stroke: 'black',
+                    strokeWidth: 4
+                })
+                circle.draggable('true')
+                layer.add(circle)
+                circle.on('dragend', (e) => {
+                    console.log('.')
+                    let pos = circle.getPosition()
+                    const _itemsTable = firebase.database().ref(`canvas/test/items/${item}`)
+                    _itemsTable.update({
+                        x: pos.x,
+                        y: pos.y
+                    })
+                })
+            }
+        }
+
+        stage.off('click')
+        stage.on('click', (e) => {
+            console.log(e.evt)
+            if(e.evt.shiftKey)
+                return
+            const _itemsTable = firebase.database().ref(`canvas/test/items/`)
+            _itemsTable.push({
+                x: e.evt.layerX,
+                y: e.evt.layerY
+            })
+        })        
+
+        stage.destroyChildren()
+        stage.add(layer)
+        stage.draw()
+    }
+
+    useEffect(() => {
+        function handleResize() {
+            //console.log('resized to: ', window.innerWidth, 'x', window.innerHeight)
+            stage?.size({
+                width: containerRef.current.clientWidth,
+                height: containerRef.current.clientHeight
+            })
+        }
+    
+        window.addEventListener('resize', handleResize)
+    })
 
     const removeItem = (itemId) => {
         const itemRef = firebase.database().ref(`/users/${user.uid}/items/${itemId}`);
@@ -77,7 +156,6 @@ const App = () => {
     const logout = () => {
         auth.signOut().then(() => {
             setUser(null)
-            setItems(items)
         })
     }
 
@@ -117,8 +195,9 @@ const App = () => {
             </header>
             {user ?
                 <div>
-                    <div className='container'>
-                        <Canvas user={user} settings={settings}/>
+                    <div className='container' ref={containerRef}>
+                        {/* <Canvas user={user} settings={settings}/> */}
+                        <div id="canvas"></div>
                         <Toolbar callback={useToolbar} settings={settings}/>
                     </div>
                 </div>
